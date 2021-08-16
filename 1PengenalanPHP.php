0e6ebbfa9fec0a2a741807f4e798a9be8267e3b2
Andreas Sebayang <79214343+andreassebayang@users.noreply.github.com>
2021-08-16 22:51:18 +0700

    Menambahkan Penjelasa

---
+++ 1PengenalanPHP.php
@@ -0,0 +1,14 @@
+<?php
+// PHP singakatan dari PHP Hypertext Processor
+// PHP banyak digunakan sebagai bahasa pemograman yang dikhususkan untuk web development
+// Bersifat Open Source
+// PHP dibuat dengan bahasa C
+// PHP banyak digunakan untuk Server-side scripting
+// PHP bisa juga digunakan untuk membuat program berbasis command line
+// extension file PHP adalah .php 
+// diawal kode program PHP wajib menambahkan  <?php dan diakhiri dengan 
+// Untuk menampilkan program PHP dapat menggunakan echo
+
+echo "Hello World";
+
+?>
